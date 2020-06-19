5db202f0dbff41a1d4ba03ad3b4d6dd075b476e9
Spell6inder <spell6inder@gmail.com>
2020-06-19 22:29:10 +0300

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+Symfony test blog
+=================
+
+# Installation
+```bash
+git clone git@github.com:Spell6inder/symfony-test-blog.git
+cd symfony-test-blog
+cd docker
+docker-compose up
+```
---
+++ src/src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class HomeController
+ *
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="")
+     * @Template
+     */
+    public function index()
+    {
+        return [
+        ];
+    }
+}
